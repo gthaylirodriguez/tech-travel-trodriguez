b077831bb8d812c794adc81d8b8dc91c114d3368
gthaylirodriguez <gthaylirodriguez@gmail.com>
2023-02-05 17:26:17 -0500

    Cart design

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,6 +1,55 @@
 import React from 'react';
+import {MdDelete} from 'react-icons/md';
+import {Container, ContainerList, TravelItem, Info, Quantity, Subtotal} from './styles';
 
 function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ContainerList>
+      <TravelItem>
+        <img src="" alt= "titulo de la imagen"/>
+        <Info>
+          <p>Título</p>
+          <strong>Precio</strong>
+        </Info>
+        <Quantity readOnly type='number' value= {1} />
+        <Subtotal>
+          <p>200.00</p>
+          <button type= 'button'>
+            <MdDelete size={24} color= '#0676d9'/>
+          </button>
+        </Subtotal>
+      </TravelItem>
+      <TravelItem>
+        <img src="" alt= "titulo de la imagen"/>
+        <Info>
+          <p>Título</p>
+          <strong>Precio</strong>
+        </Info>
+        <Quantity readOnly type='number' value= {1} />
+        <Subtotal>
+          <p>200.00</p>
+          <button type= 'button'>
+            <MdDelete size={24} color= '#0676d9'/>
+          </button>
+        </Subtotal>
+      </TravelItem>
+      <TravelItem>
+        <img src="" alt= "titulo de la imagen"/>
+        <Info>
+          <p>Título</p>
+          <strong>Precio</strong>
+        </Info>
+        <Quantity readOnly type='number' value= {1} />
+        <Subtotal>
+          <p>200.00</p>
+          <button type= 'button'>
+            <MdDelete size={24} color= '#0676d9'/>
+          </button>
+        </Subtotal>
+      </TravelItem>
+      </ContainerList>
+    </Container>
+  )
 }
 export default Cart;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {MdAddShoppingCart} from 'react-icons/md'
 import { Container, List, Unit } from './styles';
-
+import api from '../../services/api';
 function Home() {
+  const [travelList, setTravelList] = useState([]);
+
+  useEffect(()=>{
+    async function getTravelList(){
+      const {data} = await api.get('/travels');
+      setTravelList(data);
+    }
+    getTravelList();
+  },[])
+
   return (
    <Container>
       <List>
+        {travelList.map ((el)=>(
         <Unit>
-          <img src='' alt='Travel' />
-          <p>Israel</p>
-          <strong>240.00</strong>
+          <img src={el.photo} alt='Travel' />
+          <p>{el.title}</p>
+          <strong>{el.price}</strong>
         
           <button type='button'>
             <div>
@@ -18,42 +29,7 @@ function Home() {
             <span>Agregar al carrito</span>
           </button>
         </Unit>
-        <Unit>
-          <img src='' alt='Travel' />
-          <p>Israel</p>
-          <strong>240.00</strong>
-        
-          <button type='button'>
-            <div>
-              <MdAddShoppingCart size={16} color='#fff'/>
-            </div>
-            <span>Agregar al carrito</span>
-          </button>
-        </Unit>
-        <Unit>
-          <img src='' alt='Travel' />
-          <p>Israel</p>
-          <strong>240.00</strong>
-        
-          <button type='button'>
-            <div>
-              <MdAddShoppingCart size={16} color='#fff'/>
-            </div>
-            <span>Agregar al carrito</span>
-          </button>
-        </Unit>
-        <Unit>
-          <img src='' alt='Travel' />
-          <p>Israel</p>
-          <strong>240.00</strong>
-        
-          <button type='button'>
-            <div>
-              <MdAddShoppingCart size={16} color='#fff'/>
-            </div>
-            <span>Agregar al carrito</span>
-          </button>
-        </Unit>
+        ))}
       </List>
    </Container>
   ); 
